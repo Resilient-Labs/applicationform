da6e91adb613806a0f28995e2e870f75c136bf56
Muigai <muigaiunaka@gmail.com>
2016-08-10 16:14:04 -0400

    still need to style this bad boy

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by Unaka Muigai on 8/9/2016.
  */
+angular.module('Application', ['ngSanitize'] )
+    .controller('AppCtrl', function ($scope) {
+
+        $scope.limit = function() {
+            console.log("keypress");
+        /*    var space = " ";
+             var textArray = [];
+             textArray.push($scope.whyYou);
+             angular.forEach(textArray, function() {
+             if ($scope.whyYou.match(' ')) {
+             $scope.count = $scope.count + 1;
+             }
+             })*/
+
+        }
+    });
