44e3546e30ba4afcdeff77affd27f89a6212baa1
Muigai <muigaiunaka@gmail.com>
2016-09-13 12:36:56 -0400

    almost done

--- js/app.js
+++ js/app.js
@@ -14,4 +14,16 @@ angular.module('Application', ['ngSanitize'] )
             console.log( statement + $scope.isValidContact() && $scope.isValidExplanation());
             return $scope.isValidContact() && $scope.isValidExplanation();
         }
+
+        $scope.showForm = function() {
+            $scope.triggerForm = false;
+            console.log($scope.triggerForm);
+            $scope.triggerForm = !$scope.triggerForm;
+            return $scope.triggerForm;
+        }
+
+        $scope.timeline = {
+            date: ['Sep 14', 'Oct 5', 'Oct 12', 'Oct 17', 'Nov 1', 'Nov 2', 'Nov 4', 'Dec 16'],
+            event: []
+        }
     });
